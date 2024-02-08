31f3b3bba8b9608612ed8bd304f8d9814fe4f455
VynnLockwood <nolanrobinson490@gmail.com>
2024-02-08 10:30:51 +0700

    Install nuxtUI

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,7 @@
         <nuxt-link to="./Page1">To Page1</nuxt-link>
         <br>
         <nuxt-link to="./Page2">To Page2</nuxt-link>
+
+        <UButton>Button</UButton>
     </div>
 </template>
