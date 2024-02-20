3a6eaad20272bc62a64dbe53309baf564a30fb99
Natch2 <natcha11952@gmail.com>
2024-02-20 21:59:35 +0700

    Update Register.vue
    Add Input Restrictions code to Register.vue

--- pages/Register.vue
+++ pages/Register.vue
@@ -6,15 +6,15 @@
         <h1> Register</h1>
         <form @submit.prevent="register" method="post">
             <label for="username">Username:</label>
-        <input id="username" v-model="username" type="text">
+            <input id="username" v-model="username" type="text" maxlength="20" required> 
         
         <label for="email">Email:</label>
-        <input id="email" v-model="email" type="email">
+        <input id="email" v-model="email" type="email" required>
         
         <label for="password">Password:</label>
-        <input id="password" v-model="password" type="password">
+        <input id="password" v-model="password" type="password" required>
         <label for="confirmpassword">Confirm Password:</label>
-        <input id="confirmpassword" v-model="password" type="password">
+        <input id="confirmpassword" v-model="confirmpassword" type="password" required>
         <button type="submit">Register</button>
         </form>
     </div>
@@ -30,10 +30,12 @@ export default{
         };
     },
     methods: {
-      register() {
+      async register() { 
+        //const { data } = await axios.get()
         if (this.password !== this.confirmPassword) {
         alert('Passwords do not match');
         return;
+        
       }
         // Handle registration logic here
         console.log('Registering user...');
@@ -42,22 +44,29 @@ export default{
         console.log('Password:', this.password);
 
       }
+    
     }
 };
 </script>
 <style>
+
 h1{
     text-align: center;
     
+}form{
+    text-align: justify;
 }
 input{
     border: solid 1px black;
     border-radius: 5px;
     margin: 5px;
-    white-space: pre-line;
+    white-space: inherit;
+    
+    
 }
-    input[type=button],
-    input[type=submit]{
+input[type=button],
+input[type=submit]
+    {
     background-color: black;
     color: white;
     padding: 10px;  
