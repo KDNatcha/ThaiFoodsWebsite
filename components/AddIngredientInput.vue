71c81afa1801aee565b787c294df0dadc9cb6e75
VynnLockwood <nolanrobinson490@gmail.com>
2024-02-22 11:19:42 +0700

    Add methods to manage data

---
+++ components/AddIngredientInput.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        '<UFormGroup label="วัตถุดิบที่ 4"> <br> <UInput placeholder="ใส่วัตถุดิบที่ 4" icon="i-heroicons-envelope" class="w-60 h-5s" /> </UFormGroup> <br>'
+    </div>
+</template>
--- pages/AddRecipe.vue
+++ pages/AddRecipe.vue
@@ -3,39 +3,45 @@
         
         <UFormGroup v-bind:label="ingre1">
             <br>
-    <UInput placeholder="ใส่วัตถุดิบที่ 1" icon="i-heroicons-envelope" class="w-60 h-5s" />
+    <UInput v-on:input="firstIngre" placeholder="ใส่วัตถุดิบที่ 1" icon="i-heroicons-envelope" class="w-60 h-5s" />
   </UFormGroup>
   <br>
 
   
   <UFormGroup label="วัตถุดิบที่ 2">
             <br>
-    <UInput placeholder="ใส่วัตถุดิบที่ 2" icon="i-heroicons-envelope" class="w-60 h-5s" />
+    <UInput v-on:input="secondIngre" placeholder="ใส่วัตถุดิบที่ 2" icon="i-heroicons-envelope" class="w-60 h-5s" />
   </UFormGroup>
   <br>
 
   <UFormGroup label="วัตถุดิบที่ 3">
             <br>
-    <UInput placeholder="ใส่วัตถุดิบที่ 3" icon="i-heroicons-envelope" class="w-60 h-5s" />
+    <UInput v-on:input="thirdIngre" placeholder="ใส่วัตถุดิบที่ 3" icon="i-heroicons-envelope" class="w-60 h-5s" />
   </UFormGroup>
   <br>
 
   <UFormGroup label="วัตถุดิบที่ 4">
             <br>
-    <UInput placeholder="ใส่วัตถุดิบที่ 4" icon="i-heroicons-envelope" class="w-60 h-5s" />
+    <UInput v-on:input="fourtIngre" placeholder="ใส่วัตถุดิบที่ 4" icon="i-heroicons-envelope" class="w-60 h-5s" />
   </UFormGroup>
   <br>
   
 
-  <div id="ingrInput"></div>
+ 
 
-  <UButton v-on:click="createP">เพิ่มวัตถุดิบ</UButton>
 
-  <UButton>บันถึก</UButton>
+ 
+  <UButton v-on:click="saveAllIngre(firstIngre, secondIngre, thirdIngre, fourtIngre)">บันถึก</UButton>
 
-  <p>{{ ingre1 }}</p>
+  <p>วัตถุดิบที่ 1 = {{ ingredient1 }}</p>
+  <p>วัตถุดิบที่ 2 = {{ ingredient2 }}</p>
+  <p>วัตถุดิบที่ 3 = {{ ingredient3 }}</p>
+  <p>วัตถุดิบที่ 4 = {{ ingredient4 }}</p>
 
         <nuxt-link to="./">To index</nuxt-link>
+
+  
+
     </div>
 </template>
 
@@ -45,28 +51,37 @@ export default{
   data() {
     return {
       ingre1: "วัตถุดิบที่ 1",
-      ingredient: 1,
-      inputIngredient: '<UFormGroup label="วัตถุดิบที่ 4"> <br> <UInput placeholder="ใส่วัตถุดิบที่ 4" icon="i-heroicons-envelope" class="w-60 h-5s" /> </UFormGroup> <br>'
+      ingredient1: '',
+      ingredient2: '',
+      ingredient3: '',
+      ingredient4: '',
+      ingredient: 1
+      //inputIngredient: '<UFormGroup label="วัตถุดิบที่ 4"> <br> <UInput placeholder="ใส่วัตถุดิบที่ 4" icon="i-heroicons-envelope" class="w-60 h-5s" /> </UFormGroup> <br>'
     }
   },
   methods: {
-    addIngredient(){
-      return this.inputIngredient
+
+    firstIngre(event){
+      this.ingredient1 = event.target.value
+
     },
 
-    outputInput(){
-      return this.inputIngredient
+    secondIngre(event){
+      this.ingredient2 = event.target.value
+
     },
+    thirdIngre(event){
+      this.ingredient3 = event.target.value
 
-    createP(){
-      var element = document.getElementById('ingrInput');
-      element.innerHTML = this.inputIngredients
     },
+    fourtIngre(event){
+      this.ingredient4 = event.target.value
 
-    ingredientCount(){
-
-    }
-  },
+    },
+  components:{
+    
+  }
+ }
 }
 
 </script>
