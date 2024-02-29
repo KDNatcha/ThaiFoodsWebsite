ad0cdf38f99fd6de522494e4ccddbab5286818b7
Natch2 <natcha11952@gmail.com>
2024-02-29 15:54:51 +0700

    Update Register.vue
    edit Register.vue

--- pages/Register.vue
+++ pages/Register.vue
@@ -1,22 +1,27 @@
 <template>
-    <div>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       
         <nuxt-link to="./">index</nuxt-link>
+        <div class="sm:mx-auto sm:w-full sm-max-w-sm">
+        <h1 class="mt-10 text-center text-2xl font-bold "> Register</h1>
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="" method="post">
+            <label for="username" class="block text-sm font-medium leading-6 ">Username:</label>
+            <input id="username" v-model="username" type="text" maxlength="20" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6">
+        
+        <label for="email" class="block text-sm font-medium leading-6 ">Email:</label>
+        <input id="email" v-model="email" type="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"> 
+        
+        <label for="password" class="block text-sm font-medium leading-6 ">Password:</label>
+        <input id="password" v-model="password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6">
 
-        <h1> Register</h1>
-        <form @submit.prevent="register" method="post">
-            <label for="username">Username:</label>
-            <input id="username" v-model="username" type="text" maxlength="20" required> 
+        <label for="confirmpassword" class="block text-sm font-medium leading-6 ">Confirm Password:</label>
+        <input id="confirmpassword" v-model="confirmpassword" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6">
         
-        <label for="email">Email:</label>
-        <input id="email" v-model="email" type="email" required>
-        
-        <label for="password">Password:</label>
-        <input id="password" v-model="password" type="password" required>
-        <label for="confirmpassword">Confirm Password:</label>
-        <input id="confirmpassword" v-model="confirmpassword" type="password" required>
-        <button type="submit">Register</button>
+        <button type="submit" class="flex w-full justify-center rounded-md">Register</button>
         </form>
+    </div>
+    </div>
     </div>
 </template>
 <script>
@@ -49,27 +54,5 @@ export default{
 };
 </script>
 <style>
-
-h1{
-    text-align: center;
     
-}form{
-    text-align: justify;
-}
-input{
-    border: solid 1px black;
-    border-radius: 5px;
-    margin: 5px;
-    white-space: inherit;
-    
-    
-}
-input[type=button],
-input[type=submit]
-    {
-    background-color: black;
-    color: white;
-    padding: 10px;  
-    
-}
 </style>
